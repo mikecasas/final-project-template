04cb1efeea73b2c8b0f49e6f59caf46c26764f70
Mike Casas <mike.casas.public@gmail.com>
2020-11-29 19:27:51 -0500

    Modied the data in the API to make sure the Client was pulling the correct data.
    'Nothing up my sleeve'

--- backend/Controllers/WeatherForecastController.cs
+++ backend/Controllers/WeatherForecastController.cs
@@ -14,7 +14,7 @@ namespace backend.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Hot", "Hotter", "Hottest"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
