4c08e2860a118c3507d3438bbb9e141e16822d04
Mike Casas <mike.casas.public@gmail.com>
2020-11-29 19:29:06 -0500

    Added a cat as a final test for everything.

---
+++ backend/Controllers/CatController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using shared;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Cat> Get()
+        {
+            var Cats = new List<Cat>();
+            Cats.Add(new Cat { Name = "Felix" });
+            Cats.Add(new Cat { Name = "Garfield" });
+
+            return Cats;
+        }
+    }
+}
